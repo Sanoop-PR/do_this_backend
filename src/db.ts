201389b767fd67f24918f3c7fb43b76e9d204be9
Sanoop-PR <sanooppr60@gmail.com>
2025-01-07 22:21:09 +0530

    changed

--- src/db.ts
+++ src/db.ts
@@ -1,15 +1,21 @@
-import mongoose from "mongoose"
- 
-const connectToDatabase = async () => {
-  try {
-    const connection = await mongoose.connect(process.env.MONGODB||'')
-    if (connection) {
-      console.log("Connection established")
-    }
-  } catch (error) {
-    console.log("error in connectToDatabase", error)
-    throw error
-  }
-}
+import mongoose from "mongoose";
 
-export default connectToDatabase
+const connectToDatabase = async (): Promise<void> => {
+    try {
+        const uri = process.env.MONGODB || "";
+        if (!uri) {
+            console.error("MongoDB connection string is missing in the environment variables.");
+            throw new Error("Missing MongoDB connection string");
+        }
+
+        // Connect to MongoDB
+        await mongoose.connect(uri);
+
+        console.log("Connection established to MongoDB");
+    } catch (error) {
+        console.error("Error in connectToDatabase:", error instanceof Error ? error.message : error);
+        throw error;
+    }
+};
+
+export default connectToDatabase;
